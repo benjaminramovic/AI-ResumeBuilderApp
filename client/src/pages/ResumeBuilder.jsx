c6dbd7df5818a2a669baa352d83ef9d248dc0de6
Benjamin <benjaminramovic02@gmail.com>
2026-01-27 01:07:48 +0100

    personal-info-added

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,10 @@
-import { ArrowLeftIcon } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
+import PersonalInfoForm from '../components/PersonalInfoForm'
 
 const ResumeBuilder = () => {
   const {resumeId} = useParams()
@@ -20,6 +21,8 @@ const ResumeBuilder = () => {
     public: 'false'
     
   })
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0)
+  const [removeBackground, setRemoveBackground] = useState(false)
   const loadExistingResume = async () => {
     const resume = dummyResumeData.find(resume => resume._id == resumeId)
     if(resume){
@@ -31,6 +34,19 @@ const ResumeBuilder = () => {
     loadExistingResume()
   }, [])
 
+  const sections = [
+    {id:"personal", name:"Personal Info", icon: User},
+    {id:"summary", name:"Summary", icon: FileText},
+    {id:"experience", name:"Experience", icon: Briefcase},
+    {id:"education", name:"Education", icon: GraduationCap},
+    {id:"projects", name:"Projects", icon: FolderIcon},
+    {id:"skills", name:"Skills", icon: Sparkles},
+
+    
+  ]
+
+   const activeSection = sections[activeSectionIndex]
+
   return (
     <div>
       <div className='max-w-7xl mx-auto  px-4 py-6'>
@@ -41,7 +57,48 @@ const ResumeBuilder = () => {
 
       </div>
       <div className='max-w-7xl mx-auto px-4 pb-8'>
-        <div className="grid lg:grid-cols-12 gap-8"></div>
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* Left panel -  Form */}
+          <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+              {/* Progress bar using activeSectionIndex */}
+              <hr className='absolute left-0 right-0 top-0 border-2 border-gray-200'/>
+              <hr className='absolute left-0 top-0 h-1 bg-gradient-to-r from-green-500 to-green-600 transition-all duration-2000 border-none'
+              style={{
+                width: `${activeSectionIndex * 100 / (sections.length - 1)}%`
+              }}/>
+              {/* Section navigation */}
+              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                <div></div>
+                <div className='flex items-center'>
+                  {activeSectionIndex !== 0 && (
+                    <button onClick={()=>setActiveSectionIndex((prev) => Math.max(prev -1, 0))} disabled={activeSectionIndex === 0} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all'>
+                      <ChevronLeft className='size-4'/> Previous
+
+                    </button>
+                  )}
+                  <button onClick={()=>setActiveSectionIndex((prev) => Math.min(prev + 1, sections.length-1))} disabled={activeSectionIndex === sections.length-1} 
+                  className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length-1 && 'opacity-50'}`}>
+                      Next <ChevronRight className='size-4'/>
+
+                    </button>
+                </div>
+              </div>
+              {/* Form Content */}
+              <div className="space-y-6">
+                    {activeSection.id === "personal" && (
+                      <PersonalInfoForm data={resumeData.personal_info} onChange={(data) => setResumeData(prev=>({...prev, personal_info:data}))} removeBackground={removeBackground}
+                      setRemoveBackground={setRemoveBackground}/>
+                    )}
+              </div>
+
+            </div>
+
+          </div>
+
+          {/* Right panel - Preview */}
+          <div></div>
+        </div>
       </div>
     </div>
   )
